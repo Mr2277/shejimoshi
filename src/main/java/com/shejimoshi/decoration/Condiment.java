bf6d19e1a800e514eb6029049659ecfa375b0bce
Mr2277 <837544713@qq.com>
2018-07-17 17:56:25 +0800

    Sington test

---
+++ src/main/java/com/shejimoshi/decoration/Condiment.java
@@ -0,0 +1,6 @@
+package com.shejimoshi.decoration;
+
+public abstract class Condiment extends Pancake{
+    public abstract String getDesc();
+
+}
---
+++ src/main/java/com/shejimoshi/decoration/FiredEgg.java
@@ -0,0 +1,13 @@
+package com.shejimoshi.decoration;
+
+public class FiredEgg  extends Condiment{
+    @Override
+    public String getDesc() {
+        return null;
+    }
+
+    @Override
+    public double price() {
+        return 0;
+    }
+}
