b9f403d411a6154b65d37f053f31393e70fd59aa
Mr2277 <837544713@qq.com>
2018-07-17 17:10:29 +0800

    Sington test

---
+++ src/main/java/com/shejimoshi/factory/one/RepositoryFactory.java
@@ -0,0 +1,26 @@
+package com.shejimoshi.factory.one;
+
+public class RepositoryFactory {
+    public Repository create(String type) {
+        Repository repository = null;
+        switch (type) {
+            case "db":
+                repository = new DatabaseRepository();
+                break;
+            case "cache":
+                repository = new CacheRepository();
+                break;
+            case "file":
+                repository = new FileRepository();
+                break;
+        }
+        return repository;
+    }
+
+    public static void main(String[] args) {
+        RepositoryFactory factory = new RepositoryFactory();
+        factory.create("db").save(new Object());
+        factory.create("cache").save(new Object());
+        factory.create("file").save(new Object());
+    }
+}
