cd60e87109182cfaa7c6ce68fdeb5c7d8c00c0be
Mr2277 <837544713@qq.com>
2018-07-17 14:49:46 +0800

    Sington test

--- src/main/java/com/shejimoshi/singleton/one/Singleton.java
+++ src/main/java/com/shejimoshi/singleton/one/Singleton.java
@@ -2,7 +2,7 @@ package com.shejimoshi.singleton.one;
 
 public class Singleton {
     private static Singleton singleton;
-    private static Singleton newInstance(){
+    public static Singleton newInstance(){
         if(singleton == null){
            singleton= new Singleton();
         }
