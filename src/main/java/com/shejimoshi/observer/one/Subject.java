9262c31a5534fcaf02d5e641125e1277c62220d6
Mr2277 <837544713@qq.com>
2018-07-18 11:28:19 +0800

    Sington test

---
+++ src/main/java/com/shejimoshi/observer/one/Subject.java
@@ -0,0 +1,29 @@
+package com.shejimoshi.observer.one;
+
+public class Subject {
+    //既然是老大，那就必须要有小弟
+    private List<Observer> observer = new ArrayList<Observer>();
+    //老大的状态
+    private int state;
+
+    public int getState(){
+        return state;
+    }
+    //当老大的状态发生改变的时候，得通知小弟去搞事情吧
+    public void setState(int state){
+        this.state = state;
+        notifyAllObserver();
+    }
+    //每一天都有要新加入的小弟
+    public void attach(Observer observer){
+        this.observer.add(observer);
+
+    }
+    //有公告要通知每个小弟
+    public void notifyAllObserver(){
+        for(Observer observer : this.observer){
+            observer.update();
+        }
+    }
+
+}
