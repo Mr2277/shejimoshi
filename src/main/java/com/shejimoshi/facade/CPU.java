5877d8b92e5b7dd08b4be4cabb440e4825cfa119
Mr2277 <837544713@qq.com>
2018-07-21 10:24:43 +0800

    Sington test

---
+++ src/main/java/com/shejimoshi/facade/CPU.java
@@ -0,0 +1,5 @@
+package com.shejimoshi.facade;
+
+public class CPU {
+  
+}
---
+++ src/main/java/com/shejimoshi/iterator/Iterator.java
@@ -0,0 +1,6 @@
+package com.shejimoshi.iterator;
+
+public interface Iterator {
+    public boolean hasNext();
+     public Object next();
+}
