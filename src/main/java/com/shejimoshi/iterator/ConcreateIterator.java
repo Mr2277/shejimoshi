1c802d080025b9e118280983d2bad3d249650e78
Mr2277 <837544713@qq.com>
2018-07-21 10:28:23 +0800

    Sington test

--- src/main/java/com/shejimoshi/iterator/ConcreateIterator.java
+++ src/main/java/com/shejimoshi/iterator/ConcreateIterator.java
@@ -1,13 +1,28 @@
 package com.shejimoshi.iterator;
 
+import java.util.List;
+
 public class ConcreateIterator implements Iterator{
+     private List list = null;
+     private int index;
+     public ConcreateIterator(List list){
+         this.list=list;
+         this.index=list.size();
+     }
     @Override
     public boolean hasNext() {
-        return false;
+         if(index<=list.size()){
+             return true;
+         }
+         else{
+             return false;
+         }
     }
 
     @Override
     public Object next() {
-        return null;
+        Object object = list.get(index);
+                index++;
+                return object;
     }
 }
