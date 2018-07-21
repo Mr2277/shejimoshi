ca42d375bfba05bb7a9e419cb20038267af43023
Mr2277 <837544713@qq.com>
2018-07-21 10:25:36 +0800

    Sington test

---
+++ src/main/java/com/shejimoshi/iterator/ConcreateIterator.java
@@ -0,0 +1,13 @@
+package com.shejimoshi.iterator;
+
+public class ConcreateIterator implements Iterator{
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return null;
+    }
+}
