53237517a3a86c7e3488c04192208e659a0c427b
Mr2277 <837544713@qq.com>
2018-07-27 10:44:16 +0800

    Sington test

---
+++ src/main/java/com/shejimoshi/composite/Leaf.java
@@ -0,0 +1,29 @@
+package com.shejimoshi.composite;
+
+public class Leaf extends Component {
+    @Override
+    public void add(Component c) {
+        //异常处理或错误提示
+
+    }
+
+    @Override
+    public void remove(Component c) {
+
+        //异常处理或错误提示
+
+    }
+
+    @Override
+    public Component getChild(int i) {
+        //异常处理或错误提示
+
+        return null;
+    }
+
+    @Override
+    public void operation() {
+        //叶子构件具体业务方法的实现
+
+    }
+}
