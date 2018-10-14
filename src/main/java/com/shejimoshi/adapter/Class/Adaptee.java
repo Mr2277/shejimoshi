abdc87f3d93e683691c6064f5003d857d7293929
root <master>
2018-10-14 18:56:49 +0800

    fadfadfadfa

--- src/main/java/com/shejimoshi/adapter/Class/Adaptee.java
+++ src/main/java/com/shejimoshi/adapter/Class/Adaptee.java
@@ -3,6 +3,19 @@ package com.shejimoshi.adapter.Class;
 public class Adaptee {
     public void specificRequest() {
         System.out.println("被适配类...我是两孔插座  具有特殊功能");
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 }
